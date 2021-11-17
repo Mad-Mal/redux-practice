caf37e465e40de5ee6515408fdce715d90ce5d46
Mallory <mallorydoescoding@gmail.com>
2021-11-17 08:35:04 -0600

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,50 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+//State for Counter component
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
+    }
+});
+
+//State for Auth component
+const initialAuthState = {}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        },
+    },
+});
+
+const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer,
+    }
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+export default store;
